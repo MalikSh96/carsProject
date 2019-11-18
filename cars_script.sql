7d58b0bfe06542e816efa5682990295e182ff9eb
MalikSh96 <malik96sharfo@hotmail.com>
2019-11-18 20:09:13 +0100

    first commit

---
+++ cars_script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE  IF NOT EXISTS `cars`;
+USE `cars`;
+DROP TABLE IF EXISTS `information`;
+
+CREATE TABLE `information` (
+	`id` int(11) NOT NULL auto_increment,
+	`design` varchar(45), #<-- hvilken type mærke
+    `design_model` varchar(45),
+    `fuel` varchar(45),
+    `model_year` varchar(45),
+    `kilometers` int(11),
+    `color` varchar(45),
+    `steering_type` varchar(45), #f.eks servostyring
+    `gear_type` varchar(45),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  inlude_once 'Datalayer/connection.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Cars test</title>
+</head>
+<body>
+  <?php
+    print "test";
+    // $sql = "select * from information;";
+    // $result = mysqli_query($conn, $sql);
+    // $resultCheck = mysqli_num_rows($result); //checks for data
+    //
+    // if($resultCheck > 0){
+    //   while($row = mysqli_fetch_assoc($result)){
+    //     //mysqli_fetch_assoc() gets all results
+    //     //$row becomes and array due to ^
+    //     echo $row['modelyear'];
+    //   }
+    // }
+   ?>
+</body>
+</html>
