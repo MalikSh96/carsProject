9ff2219af06237d7a1f0c311b36b8fd822dbaf1f
MalikSh96 <malik96sharfo@hotmail.com>
2019-11-25 13:06:15 +0100

    Register works as it should now!

--- viewlayer/Register.php
+++ viewlayer/Register.php
@@ -15,14 +15,14 @@ $lastname = "";
 $password = "";
 
 //Use the confirm password later once the entire registration is fixed
-//$confirm_password = "";
+$confirm_password = "";
 
 $email_err = "";
 $firstname_err = "";
 $lastname_err = "";
 $password_err = "";
 
-//$confirm_password_err = "";
+$confirm_password_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -53,8 +53,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $email_err = "Please enter an email";
   }
 
+  //Validate confirm password
+  if(empty(trim($_POST["confirm_password"]))){
+      $confirm_password_err = "Please confirm password.";
+  }
+
   //If every form is filled we go down here
-  if(empty($email_err) && empty($password_err) && empty($firstname_err) && empty($lastname_err)){
+  if(empty($email_err) && empty($password_err) && empty($firstname_err)
+&& empty($lastname_err) /*&& empty($confirm_password_err)*/){
     // Check input errors before inserting in database
       $email = trim($_POST["email"]);
       //Trying to check if an email is already existing
@@ -69,6 +75,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           $password_err = "Password must have atleast 6 characters.";
         } else{
           $password = trim($_POST["password"]);
+
+          /*$confirm_password = trim($_POST["confirm_password"]);
+          if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+          }*/
+
           //If everyhing is correct we create the user
           createUserHandler($email, $firstname, $lastname, $password);
           //redirecting after registering a user
@@ -76,89 +88,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           //die;
         }
       }
-    // //If everyhing is correct we create the user
-    // createUserHandler($email, $firstname, $lastname, $password);
-    // //redirecting after registering a user
-    // header("location: Login.php");
   }
-
-
-    // else{
-    //     // Prepare a select statement
-    //     $sql = "SELECT id FROM users WHERE email = ?";
-    //
-    //     if($stmt = mysqli_prepare($conn, $sql)){
-    //         // Bind variables to the prepared statement as parameters
-    //         mysqli_stmt_bind_param($stmt, "s", $param_email);
-    //
-    //         // Set parameters
-    //         $param_email = trim($_POST["email"]);
-    //
-    //         // Attempt to execute the prepared statement
-    //         if(mysqli_stmt_execute($stmt)){
-    //             /* store result */
-    //             mysqli_stmt_store_result($stmt);
-    //
-    //             if(mysqli_stmt_num_rows($stmt) == 1){
-    //                 $email_err = "This email is already taken.";
-    //             } else{
-    //                 $email = trim($_POST["email"]);
-    //             }
-    //         } else{
-    //             echo "Oops! Something went wrong. Please try again later.";
-    //         }
-    //     }
-    //
-    //     // Close statement
-    //     mysqli_stmt_close($stmt);
-    // }
-    //
-    // // Validate password
-    // if(empty(trim($_POST["password"]))){
-    //     $password_err = "Please enter a password.";
-    // } elseif(strlen(trim($_POST["password"])) < 6){
-    //     $password_err = "Password must have atleast 6 characters.";
-    // } else{
-    //     $password = trim($_POST["password"]);
-    // }
-    //
-    // // Validate confirm password
-    // if(empty(trim($_POST["confirm_password"]))){
-    //     $confirm_password_err = "Please confirm password.";
-    // } else{
-    //     $confirm_password = trim($_POST["confirm_password"]);
-    //     if(empty($password_err) && ($password != $confirm_password)){
-    //         $confirm_password_err = "Password did not match.";
-    //     }
-    // }
-    //
-    // // Check input errors before inserting in database
-    // if(empty($email_err) && empty($password_err) && empty($confirm_password_err)){
-    //
-    //     // Prepare an insert statement
-    //     $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
-    //
-    //     if($stmt = mysqli_prepare($conn, $sql)){
-    //         // Bind variables to the prepared statement as parameters
-    //         mysqli_stmt_bind_param($stmt, "ss", $param_email, $param_password);
-    //
-    //         // Set parameters
-    //         $param_email = $email;
-    //         $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-    //
-    //         // Attempt to execute the prepared statement
-    //         if(mysqli_stmt_execute($stmt)){
-    //             // Redirect to login page
-    //             header("location: login.php");
-    //         } else{
-    //             echo "Something went wrong. Please try again later.";
-    //         }
-    //     }
-    //
-    //     // Close statement
-    //     mysqli_stmt_close($stmt);
-    // }
-
     // Close connection
     mysqli_close($conn);
 }
